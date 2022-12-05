7fb9f344548b37416f13c1891c595d902c357152
andymarlow <andymarlow4@gmail.com>
2022-12-05 12:51:20 +0000

    states created

--- weather-forecast/pages/index.js
+++ weather-forecast/pages/index.js
@@ -1,10 +1,16 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useState } from "react";
-import { BsSearch } from "react-icons";
+import { BsSearch } from "react-icons/bs";
 import axios from "axios";
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${NEXT_PUBLIC_WEATHER_KEY}`;
+
   return (
     <div>
       <Head>
