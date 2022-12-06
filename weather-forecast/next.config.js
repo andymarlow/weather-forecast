2241ef31ec271292765db0cafb75fbc2b1200454
andymarlow <andymarlow4@gmail.com>
2022-12-06 22:40:30 +0000

    config

--- weather-forecast/next.config.js
+++ weather-forecast/next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+  image: {
+    domains: ["images.unsplash.com"],
+  },
+};
--- weather-forecast/pages/index.js
+++ weather-forecast/pages/index.js
@@ -9,7 +9,23 @@ export default function Home() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${NEXT_PUBLIC_WEATHER_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setWeather(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setCity("");
+    setLoading(false);
+  };
 
   return (
     <div>
@@ -17,6 +33,11 @@ export default function Home() {
         <title>Create Next App</title>
         <meta name="description" />
       </Head>
+      <Image
+        src="https://images.unsplash.com/photo-1493314894560-5c412a56c17c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        layout="fill"
+      />
+      <button onClick={fetchWeather}>Find Weather Data</button>
     </div>
   );
 }
