16297df607d276c117bf96d1c09938bd23c07657
andymarlow <andymarlow4@gmail.com>
2023-01-16 19:45:21 +0000

    form + dynamic url + background image

---
+++ weather-forecast/pages/components/Weather.jsx
@@ -0,0 +1,5 @@
+const Weather = ({ data }) => {
+  return <div>Weather</div>;
+};
+
+export default Weather;
--- weather-forecast/pages/index.js
+++ weather-forecast/pages/index.js
@@ -3,13 +3,14 @@ import Image from "next/image";
 import { useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import axios from "axios";
+import Weather from "./components/Weather";
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
   const fetchWeather = (e) => {
     e.preventDefault();
@@ -18,7 +19,6 @@ export default function Home() {
       .get(url)
       .then((response) => {
         setWeather(response.data);
-        console.log(response.data);
       })
       .catch((err) => {
         console.log(err);
@@ -28,16 +28,31 @@ export default function Home() {
   };
 
   return (
-    <div>
+    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img">
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]" />
       <Head>
         <title>Create Next App</title>
         <meta name="description" />
       </Head>
-      <Image
-        src="https://images.unsplash.com/photo-1493314894560-5c412a56c17c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        layout="fill"
-      />
-      <button onClick={fetchWeather}>Find Weather Data</button>
+
+      <div className="relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 text-white z-10">
+        <form
+          onSubmit={fetchWeather}
+          className="relative flex justify-between items-center max-w-[500] w-full m-auto pt-4 text-white z-10"
+        >
+          <div>
+            <input
+              onChange={(e) => setCity(e.target.value)}
+              className="bg-transparent border-none text-white focus:outline-none text-2xl"
+              type="text"
+              placeholder="Enter City"
+            />
+            <br></br>
+            <button onClick={fetchWeather}>Find Weather Data</button>
+          </div>
+        </form>
+      </div>
+      {weather.main && <Weather data={weather} />}
     </div>
   );
 }
